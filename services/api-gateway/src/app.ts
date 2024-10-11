3c0dbbaf988505c8ce0b7a3f01267246323e6f54
MassDevs <samuelzentou@gmail.com>
2024-10-11 14:57:28 +0200

    WIP prod mode for docker

--- services/api-gateway/src/app.ts
+++ services/api-gateway/src/app.ts
@@ -9,3 +9,9 @@ const port = 8000;
 app.listen(port, async () => {
     console.log(`API-Gateway running on port ${port}`);
 });
+
+
+app.get("/health", (req, res) => {
+    console.log("health test");
+    res.status(200).send("Auth Service Healthy");
+});
